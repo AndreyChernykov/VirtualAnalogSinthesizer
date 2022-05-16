57614bf0170ae8522fc1e8e48ff384549d9ebc48
AndreyChernykov <totktogovoryt@yandex.ru>
2022-05-16 15:04:06 +0300

    filtrs

--- Synthesizer/Assets/Scripts/KeyNote.cs
+++ Synthesizer/Assets/Scripts/KeyNote.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class KeyNote : MonoBehaviour
 {
     [SerializeField] Slider sliderDist;//слайдер дисторшена
+    [SerializeField] TextMeshProUGUI textDist;
     [SerializeField] Slider sliderDelay;
+    [SerializeField] TextMeshProUGUI textDelay;
     [SerializeField] Slider sliderDecay;
+    [SerializeField] TextMeshProUGUI textDecay;
     private Filters filters;//скрипт фильтров
     PlayNote playNote;
 
@@ -16,6 +20,7 @@ public class KeyNote : MonoBehaviour
         playNote = gameObject.GetComponent<PlayNote>();
 
         filters = GameObject.Find("OsciliatorSinus").GetComponent<Filters>();
+
     }
 
     public void ClickKeyNote(string nameKey)//обрабатываем нажатие клавиши с нотой и получаем ее название
@@ -32,19 +37,23 @@ public class KeyNote : MonoBehaviour
         playNote.StopPlayNote();
     }
 
-    private void SliderDist()//дисторшен
+    public void SliderDist()//дисторшен
     {
         filters.DistValue = sliderDist.value;
+        textDist.text = "Dist " + (sliderDist.value * 100).ToString("0");
         filters.Distortion();//дисторшен фильтр
     }
 
-    private void SliderDelay()
+
+    public void SliderDelay()
     {
+        textDelay.text = "Delay " + sliderDelay.value.ToString("0");
         filters.Delay = sliderDelay.value;
     }
 
-    private void SliderDecay()
+    public void SliderDecay()
     {
         filters.Decay = sliderDecay.value;
+        textDecay.text = "Decay " + (sliderDecay.value * 100).ToString("0");
     }
 }
