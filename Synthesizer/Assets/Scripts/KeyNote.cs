68328daa373d00ee84901ffc74857d3cf9bd3e59
AndreyChernykov <totktogovoryt@yandex.ru>
2022-10-28 22:48:52 +0300

    q

--- Synthesizer/Assets/Scripts/KeyNote.cs
+++ Synthesizer/Assets/Scripts/KeyNote.cs
@@ -331,9 +331,10 @@ public class KeyNote : MonoBehaviour
 
     public void ClickHelp()//нажатие на кнопку с вопросом
     {
-        interAd.Show();
+        //interAd.Show();
 
-        if(interAd.IsGo)SceneManager.LoadScene("Help");
+        //if(interAd.IsGo)
+        SceneManager.LoadScene("Help");
     }
 
 }
